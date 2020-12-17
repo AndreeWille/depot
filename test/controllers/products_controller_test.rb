a0a622dd74dd545363026e17a15cde3744774a8a
Andree Wille <AndreeWille@users.noreply.github.com>
2020-12-17 15:05:41 +0100

    Add validations. TASK B Validation and Unit Testing

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -16,8 +16,9 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should create product" do
+    title = "The Great Book #{rand(1000)}"
     assert_difference('Product.count') do
-      post products_url, params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title } }
+      post products_url, params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: title } }
     end
 
     assert_redirected_to product_url(Product.last)
