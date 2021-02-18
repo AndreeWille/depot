965d1522f038f908c9ed6c4a9feade3fe3c5be28
Andree Wille <AndreeWille@users.noreply.github.com>
2021-02-18 12:59:00 +0100

    add Iteration D3: Adding a Button

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -17,10 +17,10 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
 
   test 'should create line_item' do
     assert_difference('LineItem.count') do
-      post line_items_url, params: { line_item: { cart_id: @line_item.cart_id, product_id: @line_item.product_id } }
+      post line_items_url, params: { product_id: @line_item.product_id }
     end
 
-    assert_redirected_to line_item_url(LineItem.last)
+    assert_redirected_to cart_url(Cart.last)
   end
 
   test 'should show line_item' do
@@ -44,5 +44,16 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
     end
 
     assert_redirected_to line_items_url
+  end
+
+  test 'Should create line_item' do
+    assert_difference('LineItem.count') do
+      post line_items_url, params: { product_id: products(:ruby).id }
+    end
+
+    follow_redirect!
+
+    assert_select 'h2', 'Your Pragmatic Cart'
+    assert_select 'li', products(:ruby).title.to_s
   end
 end
