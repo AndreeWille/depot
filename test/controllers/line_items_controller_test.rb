eeb2d8029b9bd74b649c41ac5df5b153e7dd33c6
Andree Wille <AndreeWille@users.noreply.github.com>
2021-03-04 14:00:19 +0100

    add iteration F3: highlighting changes

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -48,4 +48,13 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
     assert_select 'h2', 'Your Cart'
     assert_select 'td', products(:ruby).title
   end
+
+  test 'creates line items via ajax' do
+    assert_difference('LineItem.count') do
+      post line_items_url, params: { product_id: products(:ruby).id }, xhr: true
+    end
+
+    assert_response :success
+    assert_match(/<tr class=\\"line-item-highlight/, @response.body)
+  end
 end
