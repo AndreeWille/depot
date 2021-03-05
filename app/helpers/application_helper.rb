43bc8de899d9ac4ead933fdf21959ba02eda2aa9
Andree Wille <AndreeWille@users.noreply.github.com>
2021-03-05 10:18:10 +0100

    add iteration F4: hiding an empty cart with custom helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
+# frozen_string_literal: true
+
+# Defines Helper Methods available to the ApplicationController
 module ApplicationHelper
+  def render_if(condition, record)
+    render record if condition
+  end
 end
